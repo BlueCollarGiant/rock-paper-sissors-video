ac0a8870295ab692f0bd37a36376ae691b84de36
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-20 11:42:25 -0600

    taking break got stuck on bug

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+/*if (false) {
+    console.log ("hello")
+} else {
+    console.log("else")
+}*/
+/*
+let age= 15;
+
+if ( age >= 16) {
+    console.log("you can drive")
+} else if (age >= 15) {
+    console.log("you can get your permit")
+} else {
+    console.log("you can not drive")
+} */
+
+
