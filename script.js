8f26365e36d774292fd05213996de1e42b0567bf
BluecollarGiant <Mr.isaiahratliff@gmail.com>
2025-01-20 12:17:00 -0600

    finished turorial 6

--- script.js
+++ script.js
@@ -15,5 +15,20 @@ if ( age >= 16) {
 } else {
     console.log("you can not drive")
 } */
+/*
+if (5){
+    console.log('truthy')
+}
+const cartQuality = 5;
+if (cartQuality){
+    console.log( 'cart has products')
+}
 
+console.log("text" / 5)
+*/
 
+const result = true ? "truthy" : "falsy";
+console.log(result)
+
+const currency = undefined || "USD";
+console.log(currency)
